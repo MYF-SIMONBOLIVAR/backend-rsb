fa4c28135850ea632e0f8aeef11a3d48d457af7c
MUELLES Y FRENOS SIMON BOLIVAR <tic3@repuestossimonbolivar.com>
2026-02-14 15:02:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: "notificacionesticsimonbolivar@gmail.com",
         pass: "tvva tbwn hpjn lvwz" 
@@ -303,6 +303,7 @@ app.post('/api/solicitudes', upload.single('cotizacion'), (req, res) => {
 app.listen(PORT, () => {
     console.log(` Servidor RSB corriendo en http://localhost:${PORT}`);
 });
+
 
 
 
