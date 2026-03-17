6bf736bb977e7a64d142a4b40373736e5901b1b6
MUELLES Y FRENOS SIMON BOLIVAR <tic3@repuestossimonbolivar.com>
2026-03-17 11:55:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306,
+    port: process.env.DB_PORT,
     connectTimeout: 5000 // Solo 5 segundos de espera
 };
 
