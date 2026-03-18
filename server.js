1e7f10cec3979e143e1cbcbe53a2463dd19e24cf
MUELLES Y FRENOS SIMON BOLIVAR <tic3@repuestossimonbolivar.com>
2026-03-17 21:57:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 // --- MIDDLEWARES ---
 app.use(cors({
-    origin: '*', 
+    origin: 'https://compras.repuestossimonbolivar.com', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
 }));
